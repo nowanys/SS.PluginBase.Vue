b757c22d65fdb57597e691f7d099afc7402f8d97
starlying <starlying@gmail.com>
2019-05-06 11:42:41 +0800

    swal2

--- assets/js/swal2.js
+++ assets/js/swal2.js
@@ -3,3 +3,33 @@ var swal2 = swal.mixin({
   cancelButtonClass: 'btn btn-default ml-2',
   buttonsStyling: false,
 });
+
+swal2.success = function(title) {
+  swal2({
+    toast: true,
+    type: 'success',
+    title: title,
+    showConfirmButton: false,
+    timer: 2000
+  });
+};
+
+swal2.alertDelete = function (config) {
+	if (!config) return false;
+
+	swal2({
+		title: config.title,
+		text: config.text,
+		type: 'question',
+		confirmButtonText: '确认删除',
+		confirmButtonClass: 'btn btn-danger',
+		showCancelButton: true,
+		cancelButtonText: '取 消'
+	}).then(function (result) {
+		if (result.value) {
+			config.callback();
+		}
+	});
+
+	return false;
+};
--- assets/js/utils.js
+++ assets/js/utils.js
@@ -114,25 +114,5 @@ var utils = {
       },
       anim: 5
     });
-  },
-
-  alertDelete: function (config) {
-    if (!config) return false;
-
-    swal2({
-      title: config.title,
-      text: config.text,
-      type: 'question',
-      confirmButtonText: '确认删除',
-      confirmButtonClass: 'btn btn-danger',
-      showCancelButton: true,
-      cancelButtonText: '取 消'
-    }).then(function (result) {
-      if (result.value) {
-        config.callback();
-      }
-    });
-
-    return false;
   }
 };
